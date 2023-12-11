54b73ec5ab2390b50ccca1f37ad8677b25387ff9
Elliot Nelson <nelson.work@gmail.com>
2023-12-11 08:23:59 -0500

    Fix examples for pixel art canvas resize

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -3,5 +3,5 @@ module.exports = {
 		"posts"
 	],
 	"layout": "layouts/post.njk",
-	"permalink": "{{ title | slug }}"
+	"permalink": "{{ page.fileSlug | slug }}/"
 };
--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -14,11 +14,14 @@ module.exports = eleventyConfig => {
 	eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, widths, sizes) {
 		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
 		// Warning: Avif can be resource-intensive so take care!
-		let formats = ["avif", "webp", "auto"];
+		let formats = [/*"avif",*/ "webp", "auto"];
 		let file = relativeToInputPath(this.page.inputPath, src);
 		let metadata = await eleventyImage(file, {
 			widths: widths || ["auto"],
 			formats,
+			sharpOptions: {
+				animated: true
+			},
 			outputDir: path.join(eleventyConfig.dir.output, "img"), // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
 		});
 
@@ -30,5 +33,22 @@ module.exports = eleventyConfig => {
 			decoding: "async",
 		};
 		return eleventyImage.generateHTML(metadata, imageAttributes);
+	});
+
+	eleventyConfig.addAsyncShortcode("imageUrl", async function imageUrlShortcode(src, alt, widths, sizes) {
+		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
+		// Warning: Avif can be resource-intensive so take care!
+		let formats = [/*"avif",*/ "webp", "auto"];
+		let file = relativeToInputPath(this.page.inputPath, src);
+		let metadata = await eleventyImage(file, {
+			widths: widths || ["auto"],
+			formats,
+			sharpOptions: {
+				animated: true
+			},
+			outputDir: path.join(eleventyConfig.dir.output, "img"), // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
+		});
+
+		return metadata.png[0].url;
 	});
 };
