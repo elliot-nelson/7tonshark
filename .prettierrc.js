37136256e354c654e69f59c12bae20c7458190d9
Elliot Nelson <elliot.nelson@wbd.com>
2026-02-03 12:43:27 -0500

    Claude ADR Pattern

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 110,
+  endOfLine: 'auto',
+  singleQuote: true,
+  trailingComma: 'none',
+  plugins: [
+    './node_modules/prettier-plugin-packagejson/lib/index.cjs'
+  ]
+};
